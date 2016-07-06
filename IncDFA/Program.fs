de0c0e69852667069a4343de612de0fb6b58948b
Vyacheslav Chernykh <femistoklov@mail.ru>
2016-07-06 15:55:07 +0800

    Add removing of string and fix some bugs

--- IncDFA/Program.fs
+++ IncDFA/Program.fs
@@ -1,293 +1,178 @@
 ﻿open System
 
-type DFA = { Σ : Set<char>;
-             Q : Set<int>;
-             q0 : int;
-             F : Set<int>;
-             δ : Map<int * char, int> }
-             //with member this. = (this.Name, x.Age)
-
-//type DFA(Σ : Set<char>,
-//         Q : Set<int>,
-//         q0 : int,
-//         F : Set<int>,
-//         δ : Map<int * char, int>) =
-//         
-//    ///Alphabet that is set of symbols
-//    member this.Σ = Σ
-//    
-//    ///States that are marked by int numbers
-//    member this.Q = Q
-//    
-//    ///Initial state from Q
-//    member this.q0 = q0
-//    
-//    ///Final states from Q
-//    member this.F = F
-//    
-//    ///Transitions that are mapping between pairs (state, symbol) and states
-//    //member this.Transitions = transitions
-//    member this.δ = δ
-    
-    ///<summary> Partial mapping </summary>
-    ///<param name="q"> State to start from </param>
-    ///<param name="a"> Input symbol </param>
-    ///<returns> Some state if mapping exists or None in other case </returns>
-    //member this.δ q a =
-        //this.Transitions.TryFind (q, a)
-//        Option.bind (fun q -> this.Transitions.TryFind (q, a)) q
-    
-    ///<summary> Extended mapping </summary>
-    ///<param name="q"> State to start from </param>
-    ///<param name="ω"> Input string </param>
-    ///<returns> Some state if mapping exists or None in other case </returns>
-//    member this.δ' q ω =
-//        match ω with
-//        | [] -> (Some q)
-//        | a :: x ->
-//            this.δ q a
-//            |> Option.bind (fun q_next -> this.δ' q_next x)
-    
+type DFA =
+    { Σ : Set<char>; //alphabet that is set of symbols
+      Q : Set<int>; //states that are marked by int numbers
+      q0 : int; //initial state from Q
+      F : Set<int>; //final states from Q
+      δ : Map<int * char, int> //transitions that are mapping between
+                               //pairs (state, symbol) and states
+    }
+        
     ///<summary> Trying to accept the input string </summary>
     ///<param name="ω"> Input string </param>
     ///<returns> True if ω is accepted by automaton or False in other case </returns>
-//    member this.Accept ω =
-//        this.δ' q0 ω
-//        |> Option.fold (fun _ q -> F.Contains q) false
-        
-///
-//let addState q (mappings : List<char*int>) isFinal (M : DFA) =
-//    DFA(M.Σ,
-//        M.Q.Add q,
-//        M.q0,
-//        (if isFinal then M.F.Add q else M.F),
-//        (List.fold (fun δ (a, q_next) -> Map.add (q, a) q_next δ) M.δ mappings))
-//let addState q mappings isFinal (M : DFA) =
-//    DFA(M.Σ,
-//        M.Q.Add q,
-//        M.q0,
-//        (if isFinal then M.F.Add q else M.F),
-//        List.foldBack (fun (a, q_next) ->
-//            Map.add (q, a) q_next) mappings M.δ)
-let addState q mappings isFinal M =
+    member M.Accept ω =
+        ///function returns extended mapping δ*(q, ω)
+        let rec f ω q =
+            match ω with
+            | [] -> (Some q)
+            ///return None or recursivly applies f to result of mapping
+            | a :: x -> M.δ.TryFind (q, a) |> Option.bind (f x)
+        ///finds δ*(q0, ω) and checks that result is a final state
+        f ω M.q0 |> Option.fold (fun _ -> M.F.Contains) false
+
+    ///prints automaton's specification
+    override M.ToString() =
+        sprintf "Alphabet: %A\nQ: %A \nq0: %A\nF: %A\nTransitions: " M.Σ M.Q M.q0 M.F
+        + (Map.fold (fun acc k v -> acc + sprintf "\n  %A -> %A" k v) "" M.δ)
+
+///adds given state q to automaton M. If "isFinal" is set to true,
+///also adds it to the final states
+let addState q isFinal M =
     { M with
         Q = M.Q.Add q;
-        F = (if isFinal
-             then M.F.Add q
-             else M.F);
-        δ = List.foldBack (fun (a, q_next) -> Map.add (q, a) q_next)
-                          mappings
-                          M.δ }
-    
-//let removeState q (M : DFA) = //exception q=q0
-//    DFA(M.Σ,
-//        M.Q.Remove q,
-//        M.q0,
-//        M.F.Remove q,
-//        Set.foldBack (fun a -> Map.remove (q, a)) M.Σ M.δ)
-let removeState q M = //exception q=q0
-    { M with
-        Q = M.Q.Remove q;
-        F = M.F.Remove q;
-        δ = Set.foldBack (fun a -> Map.remove (q, a)) M.Σ M.δ }
+        F = if isFinal then M.F.Add q else M.F }
 
-let addString (M:DFA) ω =
-        
-    ///returns a cloned state (with all transitions created)
-    ///if the argument is a nonabsorption state or a queue state
-    ///if it operates on the absorption state (None)
-//        let clone q =
-//            let q' = Set.maxElement this.Q + 1
-//            q |> Option.iter (fun q ->
-//                for a in this.Σ do
-//                    this.δ (Some q) a
-//                    |> Option.iter (fun q_next ->
-//                        this.Transitions <- this.Transitions.Add ((q', a), q_next))
-//                if this.F.Contains q then this.F <- this.F.Add q')
-//            this.Q <- this.Q.Add q'
-//            q'
+///removes given state q from automaton M. If q is initial state,
+///generates an InvalidOperationException
+let removeState q M =
+    if q <> M.q0
+    then { M with
+            Q = M.Q.Remove q;
+            F = M.F.Remove q;
+            δ = Set.foldBack (fun a -> Map.remove (q, a)) M.Σ M.δ }
+    else invalidOp "The initial state cannot be removed"
 
-    let R = ref M.Q
-//    let q0' = Set.maxElement M.Q + 1
-//    let passed = List.scan M.δ (Some M.q0) ω
-//    let added = [q0' .. q0' + ω.Length]
-//    let (clonedTrs, addedF) =
-//        (passed, added)
-//        ||> List.fold2
-//                (fun (trs, (sts:Set<int>)) q q' ->
-//                    match q with
-//                    | None -> trs, sts
-//                    | Some v -> 
-//                        (Seq.fold (fun trs' a ->
-//                            Option.foldBack
-//                                (Map.add (q', a))
-//                                (M.δ (Some v) a)
-//                                trs')
-//                            trs M.Σ),
-//                        (if sts.Contains v
-//                         then Set.add q' sts
-//                         else sts))
-//                (M.Transitions, M.F)
-//    let newStates = 
-//    let newF = Set.add (Set.maxElement newStates + 1) addedF
-//    let newTrs =
-//        (Seq.pairwise added, ω)
-//        ||> Seq.fold2 (fun ts (q, q_next) ω_i ->
-//                            Map.add (q, ω_i) q_next ts) clonedTrs
+///adds given transition ((q1, a), q2) to automaton M. If states q1, q2
+///or symbol a are not in the automaton, generates an ArgumentException
+let addTransition ((q1, a), q2) M =
+        if M.Q.Contains q1 && M.Q.Contains q2 && M.Σ.Contains a
+        then { M with δ = Map.add (q1, a) q2 M.δ }
+        else invalidArg "((q1, a), q2)" "The transition contains wrong elements"
 
-    ///takes state q, cloned state q', transitions δ, alphabet Σ and returns
-    ///the union of δ and all transitions coming from q where q is replaced by q'
-//    let cloneTrs q q' =
-//        Seq.fold (fun δ a ->
-//            Option.foldBack
-//                (Map.add (q', a))
-//                (δ.TryFind (q, a))
-//                δ)
+///takes boolean flag, automaton M and string ω and adds
+///or removes ω to\from M depends on the value of the flag
+let addOrRemove flag M ω =
 
-    let cloneTrs q M =
-        Set.fold (fun trs a ->
-                    match M.δ.TryFind (q, a) with
-                    | Some v -> (a, v)::trs
-                    | None -> trs)
-                 [] M.Σ
-//        [for a in M.Σ do
-//            match M.δ.TryFind (q, a) with
-//            | Some v -> yield (a, v)
-//            | None -> ()]
+    ///takes state q, cloned state q', automaton M and returns
+    ///new automaton with the union of δ and all transitions
+    ///coming from q where q is replaced by q'
+    let cloneTrs q' q M =
+        { M with δ = Set.foldBack (fun a ->
+                        Option.foldBack (Map.add (q', a))
+                                        (M.δ.TryFind (q, a)))
+                                  M.Σ M.δ }
 
-    ///takes state q, alphabet Σ, transitions δ and returns
-    ///the difference of δ and all transitions coming from q
-    let removeTrs q = Seq.foldBack (fun a -> Map.remove (q, a))
-
-    let rec clone M q q' ω added =
+    ///takes string ω, list of already created states "added", new state q',
+    ///state q, automaton M and recursivly adds q' to M for all δ*(q, ω)
+    let rec clone flag ω added q' q M =
         match ω with
-        | [] -> addState q' [] true M, q'::added
-        | ω_i::tail ->
-            let (isFinal, q_next, mps) =
-                match q with
-                | None -> false, None, []
-                | Some v -> M.F.Contains v,
-                            M.δ.TryFind (v, ω_i),
-                            cloneTrs v M
-            clone (addState q' ((ω_i, q' + 1)::mps) (isFinal) M)
-                  q_next
-                  (q' + 1)
-                  tail
-                  (q'::added)
+        //add the last of created states to automaton (and to final states
+        //if flag = true, e.g. the string ω is added to automaton)
+        | [] -> Option.foldBack (cloneTrs q') q (addState q' flag M), q'::added
+        | a :: x ->
+            match q with
+            //if q is an absorption state (e.g. None),
+            //add q' to automaton as a queue state
+            | None -> None,
+                      M |> addState q' false
+            //if q is a nonabsorption state, add q' to automaton
+            //as a cloned state of q with all transitions created
+            | Some v -> M.δ.TryFind (v, a),
+                        M |> addState q' (M.F.Contains v) |> cloneTrs q' v
+            //create new state, add q' to list of created states and call
+            ///the function again for new nonabsorption state (or None),
+            ///augmented automaton and next symbol of ω
+            ||> clone flag x (q'::added) (q' + 1)
 
-//    List.fold (fun (states, transitions, q) ->
-//        let q' = Set.maxElement states + 1
-//        let q_next = M.δ q ω_i
-//        ((Set.add q' states), (Map.add transitions, q))
-//        (M.Q, M.Transitions, q0) ω
-
-//    this.F <- this.F.Add
-//        <| List.fold (fun q_last ω_i ->
-//                let q = clone (this.δ (Some q_last) ω_i)
-//                this.Transitions <- this.Transitions.Add ((q_last, ω_i), q)
-//                q)
-//                q0' ω
-        
     ///takes state q, transitions δ and checks
-    ///whether δ has transitions incoming into q or not
+    ///whether δ has transitions incoming into q or has not
     let unreachable q = not << Map.exists (fun _ -> (=) q)
             
-    ///takes alphabet Σ, input string ω, states Q, register R,
-    ///transitions δ, start state q and recursivly removes
-    ///all unreacheable states from Q, R and δ, moving by ω
+    ///takes state q, input string ω, register R, automaton M, and
+    ///recursivly removes all unreacheable states from R and M for all δ*(q, ω)
     let rec eliminate q ω R M =
         match q with
         | Some v when unreachable v M.δ ->
             match ω with
             | [] -> Set.remove v R, removeState v M
-            | ω_i::tail -> eliminate (M.δ.TryFind (v, ω_i))
-                                     tail
-                                     (R.Remove v)
-                                     (removeState v M)
+            | a :: x -> eliminate (M.δ.TryFind (v, a))
+                                  x
+                                  (R.Remove v)
+                                  (removeState v M)
         | _ -> R, M
 
-            
-    (*ω |> Seq.scan (fun q ω_i ->
-        Option.bind(fun q ->let q_next
-         = this.δ q ω_i
-                            this.Q <- this.Q.Remove q
-                            for a in this.Σ do
-                                this.Transitions <- this.Transitions.Remove (q, a)
-                            R := (!R).Remove q
-                            q_next) q) (Some q0)*)
-//    ω
-//    |> Seq.scan (fun q ω_i ->
-//        let q_next = this.δ q ω_i
-//        this.Q <- this.Q.Remove q.Value
-//        for a in this.Σ do
-//            this.Transitions <- this.Transitions.Remove (q.Value, a)
-//        R := (!R).Remove q.Value
-//        q_next) (Some q0)
-//    |> Seq.takeWhile (Option.fold (fun _ -> unreacheable) false)
-//    |> Seq.iter (ignore)
-//    this.q0 <- q0'
-
-//        let rec clearEdges node hopList =
-//            if List.isEmpty hopList || Map.exists (fun _ dst -> dst = node) this.Transitions then this.Transitions
-//            else let tr' = Map.filter (fun (src, _) _ -> src <> node ) this.Transitions
-//                 match Map.tryFind (node, hopList.Head) this.Transitions with
-//                 | None -> tr'
-//                 | Some node -> clearEdges tr' node hopList.Tail
-
-    ///checks for the equivalence of states by comparing
-    ///(i) whether both states are accepting or not, and
-    ///(ii) whether the outgoing transitions lead to the same state in R
+    ///takes automaton M, state p from register, state q from Q
+    ///and checks for the equivalence of p and q
     let equiv M p q =
+        //both states are accepting or not
         (M.F.Contains p = M.F.Contains q)
+        //all outgoing transitions lead to the same states
         && not <| Set.exists (fun a ->
             M.δ.TryFind (q, a) <> M.δ.TryFind (p, a)) M.Σ
 
-    ///redirect into p transitions coming into q
-    let merge p q δ =
-        Map.map (fun _ -> function
-                    | q_next when q_next = q -> p
-                    | q_next -> q_next)
-                 δ
+    ///takes state p from R, state q from Q, automaton M and return new automaton
+    ///where q is removed and transitions coming into q are redirected into p
+    let merge p q M =
+        { M with
+            Q = M.Q.Remove q;
+            F = M.F.Remove q;
+            δ = Map.map (fun _ -> function
+                        | q_next when q_next = q -> p
+                        | q_next -> q_next)
+                    M.δ }
 
-    ///If the argument state q is found to be equivalent to a state p
-    ///in the register R, function merge(p; q) is called;
-    ///if not, it is simply added to the register
-    let rec repreg states R (M : DFA) =
-        match states with
+    ///takes reverse list of created cloned and queue states, register R,
+    ///automaton M and recursivly merges equivalent states from list and R
+    let rec repreg added (R : Set<int>) M =
+        match added with
         | [] -> M
-        | q::tail ->
-            match R |> Set.toSeq |> Seq.tryFind (equiv M q) with
-            | Some p -> repreg tail
-                               R
-                               { M with δ = (merge p q M.δ) }
+        | q :: tail ->
+            match Seq.tryFind (equiv M q) R with
+            //if q from added states and p from R are equivalent, merge them
+            //and call the function again for rest states and new automaton
+            | Some p -> repreg tail R (merge p q M)//{ M with δ = merge p q M.δ }
+            //if not, q is added to the register R
             | None -> repreg tail (R.Add q) M
-        
-//    for i in ω.Length .. -1 .. 1 do
-//        this.δ' (Some this.q0) (Seq.take i ω |> Seq.toList)
-//        |> Option.iter (replace_or_register)
-    //List.fold (fun q ω_i -> Option.bind (fun q -> Map.tryFind (q, ω_i) δ') q) (Some q0') ω
 
-    //let q0' = Set.maxElement M.Q + 1
-    let q0' = M.Q.MaximumElement + 1
-    let (M', added) = clone M (Some M.q0) q0' ω []
-    eliminate (Some M.q0) ω M.Q { M' with q0 = q0'}
-    ||> repreg (List.rev added)
+    let q0' = M.Q.MaximumElement + 1 //create new initial state
+    //create new automaton with cloned and queue states and list of this states
+    let (M', added) = clone flag ω [] q0' (Some M.q0) M
+    //add transitions between cloned and queue states and change initial state for q0'
+    { Seq.foldBack2 (fun a (q2, q1) ->
+        addTransition ((q1, a), q2)) (List.rev ω) (List.pairwise added) M'
+      with q0 = q0'}
+    |> eliminate (Some M.q0) ω M.Q //remove those states that became unreacheable
+    ||> repreg added //merge equivalent states and return resulting automaton
+
+///takes automaton M and string ω and returns new automaton M'
+///that is minimal, complete and such that L(M') = L(M) U {ω}
+let addString = addOrRemove true
+
+///takes automaton M and string ω and returns new automaton M'
+///that is minimal, complete and such that L(M') = L(M) \ {ω}
+let removeString = addOrRemove false
 
 [<EntryPoint>]
-let main argv = 
-    //printfn "%A" argv
-    let ts = Map.ofList [
-              (0, 'b'), 1;
-              (1, 'a'), 2;
-              (2, 'r'), 3;
-              (2, 'b'), 4;
-              (4, 'a'), 5;
-              (5, 'b'), 4;
-    ]
-    let M = { Σ = set ['a';'b';'r']; Q = set [0..5]; q0 = 0; F = set [2;3;5]; δ = ts }
-    //M.Accept ['b';'a';'b';'a'] |> ignore
-    let M' = addString M ['b';'r';'a']
-    printfn "%A" M'.Q
-    M'.δ |> Map.iter (printfn "%A -> %A")
+let main argv =
+    let M = { Σ = set ['a'; 'b'; 'r'];
+              Q = set [0 .. 5];
+              q0 = 0;
+              F = set [2; 3; 5];
+              δ = Map.ofList [ (0, 'b'), 1;
+                               (1, 'a'), 2;
+                               (2, 'r'), 3;
+                               (2, 'b'), 4;
+                               (4, 'a'), 5;
+                               (5, 'b'), 4; ] }
+    printfn "accept baba: %A" <| M.Accept ['b';'a';'b';'a']
+    printfn "accept babb: %A" <| M.Accept ['b';'a';'b';'b']
+    printfn ""
+    let M1 = addString M ['b';'r';'a']
+    printf "%O" M1
+    printfn "\n"
+    let M2 = removeString M1 ['b';'a';'b';'a']
+    printf "%O" M2
     Console.ReadLine() |> ignore
     0 // возвращение целочисленного кода выхода
